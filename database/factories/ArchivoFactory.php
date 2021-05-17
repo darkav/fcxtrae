997f26a16215b0df544df0837d35abba955643a7
darkav <avsisec@gmail.com>
2021-05-17 01:27:27 -0500

    17052021v1

---
+++ database/factories/ArchivoFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\archivo;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ArchivoFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = archivo::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ database/migrations/2021_05_15_163827_create_archivos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArchivosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('archivos', function (Blueprint $table) {
+            $table->id();
+            $table->integer('compania_id')->unsigned();
+            $table->integer('tipodoc_id')->unsigned();
+            $table->date('fecha');
+            $table->string('fichero',100);
+            $table->string('path_fichero',300);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('archivos');
+    }
+}
